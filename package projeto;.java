b69bc557b5cdb54692a897a640deac649b6a1c9e
lsantosgbe <105877565+lsantosgbe@users.noreply.github.com>
2025-01-24 16:12:29 -0300

    Add files via upload

---
+++ package projeto;.java
@@ -0,0 +1,24 @@
+package projeto;
+import java.util.Scanner;
+
+public class Myclass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num1 * num2;
+        double divisao = num1 / num2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+    }
+}
